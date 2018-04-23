1d945aa305fa7d4a5944f85d7fa154ddef9b99c6
Juber Moulvi Abdul <ma.juber@gmail.com>
2018-04-23 15:12:30 +0530

    Add files via upload

---
+++ Luhn Algorithm/Luhn Algorithm/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Luhn Algorithm
+//
+//  Created by Juber Moulvi Abdul on 23/04/18.
+//  Copyright © 2018 Juber Moulvi Abdul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("1234567890".luhnCheck())
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+extension String {
+    
+    func luhnCheck() -> Bool {
+        var sum = 0
+        let reversedCharacters = self.reversed().map { String($0) }
+        for (idx, element) in reversedCharacters.enumerated() {
+            guard let digit = Int(element) else { return false }
+            switch ((idx % 2 == 1), digit) {
+            case (true, 9): sum += 9
+            case (true, 0...8): sum += (digit * 2) % 9
+            default: sum += digit
+            }
+        }
+        return sum % 10 == 0
+    }
+}
